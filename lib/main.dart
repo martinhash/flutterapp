953457a0b6c7f4870d23cb44f949c65cdf370102
Martin Barria Joost <martinjoostw@gmail.com>
2020-04-11 15:52:57 -0600

    base

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/src/pages/home_temp.dart';
+import 'package:flutter_app/src/pages/home_page.dart';
  
 void main() => runApp(MyApp());
  
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: HomePageTemp()
+      home: HomePage()
     );
   }
 }
