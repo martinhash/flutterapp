178d3add6c4f0c4e81cd85c8ded91ddbe85e76b5
Martin Barria Joost <martinjoostw@gmail.com>
2020-04-12 09:47:51 -0600

    icons

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/src/providers/menu_provider.dart';
+import 'package:flutter_app/src/utils/iconos_string_util.dart';
 
 
 class HomePage extends StatelessWidget {
@@ -12,29 +13,43 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _lista() {
-  
-  menuProvider.cargarData().then( (opciones) {
 
-    print('_lista');
-    print(opciones);
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      initialData: [],
+      builder:( context, AsyncSnapshot<List<dynamic>> snapshot){
 
+        print('builder');
+        print(snapshot.data);
 
-  });
-  return ListView( children: _listaItems() , ); 
+        return ListView(
+          children: _listaItems( snapshot.data ),
+        );
 
+      }  ,
+    );
   }
 
-  List<Widget> _listaItems() {
+  List<Widget> _listaItems( List<dynamic> data ) {
+    
 
-    return [
-      ListTile( title: Text('Hola mundo'), ),
-      Divider(),
-      ListTile( title: Text('Hola mundo'), ),
-      Divider(),
-      ListTile( title: Text('Hola mundo'), ),
-    ];
+    final List<Widget> opciones = [];
+    data.forEach( (opt){
 
+      final widgetTemp = ListTile(
 
+        title: Text(opt['texto']),
+        leading: getIcon( opt['icon'] ) ,
+        trailing: Icon(Icons.keyboard_hide),
+        onTap: (){},
+      );
+
+    opciones..add( widgetTemp )
+            ..add(Divider());
+
+    });
+
+  return opciones;
   }
 
 }
