6be2b9856c7d2c212594fcf1ea67310ae4ecaa67
Martin Barria Joost <martinjoostw@gmail.com>
2020-04-12 10:52:21 -0600

    fix target

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -5,11 +5,44 @@ class CardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-
       appBar: AppBar( title: Text('Card'), ),
-
+      body: ListView(
+        padding: EdgeInsets.all(10),
+        children: <Widget>[
+          _cardTipo1(),
+        ],
+      ),
     );
   }
+}
+
+Widget _cardTipo1() {
+
+  return Card(
+    child: Column(
+      children: <Widget>[
+        ListTile(
+          leading: Icon(Icons.photo_album, color: Colors.blue,),
+          title: Text('Soy el Titulo'),
+          subtitle: Text('The latest tools that help you and your team build software better, together.The latest tools that help you and your team build software better, together.'),
+          
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            FlatButton(
+              child: Text('Cancelar'),
+              onPressed: (){},
+            ),
+            FlatButton(
+              child: Text('Ok'),
+              onPressed: (){},
+            )
+          ],
+        )
+      ],
+    ),
+  );
+
 }
 
