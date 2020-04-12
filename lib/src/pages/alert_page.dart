4f0351de8f835615a3d042e845e33cddf2d15e6c
Martin Barria Joost <martinjoostw@gmail.com>
2020-04-12 15:04:37 -0600

    alertdialog

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -9,10 +9,33 @@ class AlertPage extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         child: Icon( Icons.feedback ),
         onPressed: (){
+          
           Navigator.pop(context);
+
         },
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Mostrar alerta'),
+          onPressed: () => _mostrarAlerta(context),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          ),
+        ),
     );
+  }
+  void _mostrarAlerta( BuildContext context){
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context){
+        return AlertDialog(
+          title: Text('Okey'),
+          content: Text('Contenido'),
+        );
+      }
+      );
   }
 }
 
