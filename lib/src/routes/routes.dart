31b461c33be6252624d82de50ce958cf3660ab5d
Martin Barria Joost <martinjoostw@gmail.com>
2020-04-12 18:08:36 -0600

    feat(input_page)

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -7,6 +7,7 @@ import 'package:flutter_app/src/pages/animated_container.dart';
 import 'package:flutter_app/src/pages/avatar_page.dart';
 import 'package:flutter_app/src/pages/card_page.dart';
 import 'package:flutter_app/src/pages/home_page.dart';
+import 'package:flutter_app/src/pages/input_page.dart';
 
 Map<String, WidgetBuilder> getAplicationRoutes(){
 
@@ -17,6 +18,7 @@ return <String, WidgetBuilder>{
       'avatar' : ( BuildContext context )=>AvatarPage(),
       'card'   : ( BuildContext context )=>CardPage(),
       'animatedContainer'   : ( BuildContext context )=>AnimatedContainerPage(),
+      'inputs'   : ( BuildContext context )=>InputPage(),
       };
 
 }
