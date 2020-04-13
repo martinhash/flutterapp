baf68e58979b1de5169775fa97f1a9a15b3137bc
Martin Barria Joost <martinjoostw@gmail.com>
2020-04-12 19:49:00 -0600

    add(pubspec)internazionalization

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
 
 import 'package:flutter_app/src/pages/alert_page.dart';
 import 'package:flutter_app/src/routes/routes.dart';
+
  
 void main() => runApp(MyApp());
  
@@ -9,8 +12,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', 'US'),
+        const Locale('es', 'ES'),
+      ],
       initialRoute: '/',
       routes: getAplicationRoutes(),
       onGenerateRoute: ( RouteSettings settings ){
